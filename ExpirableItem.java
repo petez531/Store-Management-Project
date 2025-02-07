eec3f269ac25afb9ab25e11302d5b562326d7fef
petez531 <petez531@hsd.k12.or.us>
2025-02-07 10:16:56 -0800

    Create ExpirableItem.java
    Imported from code.org

---
+++ ExpirableItem.java
@@ -0,0 +1,59 @@
+import java.time.LocalDate;
+import java.time.DateTimeException;
+import java.time.temporal.ChronoUnit;
+
+public class ExpirableItem extends Item {
+    private LocalDate expirationDate;
+
+    public ExpirableItem() {
+        this("Default Expirable Item", 0, 0, 0, LocalDate.now().toString());
+    }
+
+    public ExpirableItem(String name, double price, int amount, double discount, String expirationDate) {
+        super(name, price, amount, discount);
+        try {
+            this.expirationDate = LocalDate.parse(expirationDate);
+        }
+        catch (DateTimeException ex) {
+            System.out.print("Unable to parse expiration date. Expiration date format is invalid. It must be in the form yyyy-mm-dd");
+            throw ex;
+        } 
+    }
+
+    public ExpirableItem(String name, double price, int amount, double discount, LocalDate expirationDate) {
+        super(name, price, amount, discount);
+        this.expirationDate = expirationDate;
+    }
+
+    public LocalDate getExpirationDate() {
+        return this.expirationDate;
+    }
+
+    public long daysUntilExpiration() {
+        if (this.isExpired()) {
+            return 0;
+        }
+        return LocalDate.now().until(this.expirationDate, ChronoUnit.DAYS);
+    }
+
+    public boolean isExpired() {
+        return this.expirationDate.isBefore(LocalDate.now());
+    }
+
+    public void setExpirationDate(String expirationDate) {
+        try {
+            this.expirationDate = LocalDate.parse(expirationDate);
+        }
+        catch (DateTimeException e) {
+            System.out.println("Unable to parse expiration date. Expiration date format is invalid. It must be in the form yyyy-mm-dd");
+        }
+    }
+
+    public void setExpirationDate(LocalDate expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public String toString() {
+        return String.format("%s\nItem Expiration Date: %tF\nIs Expired: %b%s",
+                             super.toString(), this.expirationDate, this.isExpired(), (this.isExpired() ? "" : String.format("\nDays Until Expired: %,d", this.daysUntilExpiration())));
+    }
